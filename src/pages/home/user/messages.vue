0e693072c36b28d6dffd0b1e08c5799366c7b160
Mario <zhangbiaoFT@163.com>
2018-01-12 18:00:10 +0800

    系统消息初始化

--- src/pages/home/user/messages.vue
+++ src/pages/home/user/messages.vue
@@ -1,14 +1,47 @@
 <template>
 	<div class="messages">
     <div class="msg-top">
+      <!-- <grid>
+        <grid-item>
+          1
+        </grid-item>
+        <grid-item>
+          2
+        </grid-item>
+        <grid-item>
+          3
+        </grid-item>
+      </grid> -->
+      <div class="tips">
+        <div class="tip"></div>
+        <div class="tip">
+          <div class="tip-icon">
+            <i class="iconfont">&#xe60c;</i>
+            <br>
+            <span>系统消息</span>
+          </div>
+          <div class="tip-icon">
+             <i class="iconfont">&#xe60c;</i>
+             <br>
+              <span>待办提醒</span>
+          </div>
+        </div>
+        <div class="tip"></div>
+      </div>
     </div>
 	</div>
 </template>
 <script>
+import { Grid, GridItem, GroupTitle } from 'vux'
 	export default {
 		data() {
 			return {}
-		}
+    },
+    components: {
+      Grid,
+      GridItem,
+      GroupTitle
+    }
 	}
 </script>
 
@@ -20,6 +53,51 @@
   height: 130px;
   background: #ffffff;
   margin-top: 16px;
+}
+.msg-top .tips {
+  position: relative;
+  overflow: hidden;
+}
+.msg-top .tips .tip {
+  position: relative;
+  float: left;
+  padding: 20px 10px;
+  box-sizing: border-box;
+}
+.msg-top .tips .tip:nth-child(1),.msg-top .tips .tip:nth-child(3) {
+  width: 20%;
+}
+.msg-top .tips .tip:nth-child(2) {
+  width: 60%;
+}
+.msg-top .tips .tip .tip-icon{
+  display: inline-block;
+  width: 70px;
+  height: 70px;
+  text-align: center;
+  border-radius: 10px;
+}
+.msg-top .tips .tip .tip-icon:nth-child(1){
+  background: #ff7a70;
+}
+.msg-top .tips .tip .tip-icon:nth-child(2){
+   background: #ffc146;
+   float: right;;
+}
+.msg-top .tips .tip .tip-icon i{
+  display: inline-block;
+  font-size: 40px;
+  line-height: 70px;
+  color: #ffffff;
+}
+.msg-top .tips .tip .tip-icon:nth-child(1) i{
+    transform: rotate(-45deg);
+}
+.msg-top .tips .tip .tip-icon:nth-child(2) i{
+    transform: rotate(45deg);
+} 
+.tip-icon span{
+  color: #414141;
 }
 /* .msg-top .tip{
   width: 25%;
