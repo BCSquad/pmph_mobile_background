cab7b8f46bd895c649f647dbf7c1dfac4fdd7275
AzureCa <18428368747@sina.cn>
2018-01-16 18:01:07 +0800

    移动端选题申报页面开发

---
+++ src/pages/home/selectTopic/examDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="exam_detail">
+    <ul>
+        <li>
+
+        </li>
+    </ul>
+  </div>
+</template>
+<script >
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+<style lang="less">
+@import './less/examDetail.less';
+</style>
