384fc3c92d0ee02b9b1c5779f72b06222e6d0f43
AzureCa <18428368747@sina.cn>
2018-01-29 17:44:47 +0800

    日常

--- src/pages/home/material/distributePlanEditor.vue
+++ src/pages/home/material/distributePlanEditor.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="distribute_editor">
     <x-header :left-options="{backText: ''}" class="header">分配策划编辑
-        <a slot="right" style="color:#fff;"  >完成</a>
+        <a slot="right" style="color:#fff;"  @click="submitChecked">完成</a>
      </x-header>
         
         <search 
           ref="searchBar"
           placeholder="人名、账号搜索"
           :autoFixed="false"
+          v-model="searchParams.name"
+          @on-submit="search"
         ></search>
         <ul class="check_list">
           <li  class="check_list_li"  v-for="(item,index) in treeData.sonDepartment" :key="index">
@@ -17,17 +19,18 @@
               class="wrapper_box"
               :border-intent="false"
               :arrow-direction="item.isShow ? 'down' : 'up'"
-              @click.native="getMemberList(item)" ></cell>
+              @click.native="clickDown(item)" ></cell>
               <template >
-                <div class="slide_box">
-                  <!-- <checklist 
+                <div class="slide_box" >
+                  <checklist 
+                  :title="item.realname"
                   class="check_item" 
-                  :class="{'isHideList':!showContent001}" 
+                  v-if="item.isShow"
                   label-position="right" 
                   required 
-                  :options="commonList" 
-                  v-model="checklist001" 
-                  @on-change="change"></checklist>    -->  
+                  :options="item.childrenData" 
+                  v-model="item.Checked" 
+                  @on-change="change"></checklist>     
 
                   </div>            
               </template>
@@ -41,11 +44,19 @@ import { Cell,CellBox,XHeader,Search,Group,Checklist  } from 'vux'
      data(){
        return{
           departmentTreeUrl:'/pmpheep/departments/tree', //获取部门树url
+          memberListUrl:'/pmpheep/users/pmph/list/pmphUser',  //  部门成员url
           treeData:{},
           showContent:true,
           checklist001:[],
           commonList:[ 'China', 'Japan', 'America' ],
-
+          searchParams:{
+                name:'',
+                path:'',
+                departmentId:'',
+                pageNumber:1,
+                pageSize:100
+          },
+          currentItem:{}
        }
      },
      components: {
@@ -67,17 +78,71 @@ import { Cell,CellBox,XHeader,Search,Group,Checklist  } from 'vux'
              var  arr=res.data.data.sonDepartment;
              for(var i in arr){
                arr[i].isShow=false;
+               arr[i].Checked=[];
+               arr[i].childrenData=[];
              }
              this.treeData=res.data.data;
            }
          })
        },
-       getMemberList(obj){
-        obj.isShow=!obj.isShow;
-        
+       /* 点击下拉 */
+       clickDown(item){
+         /* 关闭其他列 */
+            for(var i in this.treeData.sonDepartment){
+              this.treeData.sonDepartment[i].isShow=false;
+            }
+            item.isShow=!item.isShow;
+            this.currentItem=item;
+          if(!item.isShow||item.childrenData.length!=0){
+            /* item.childrenData=[]; */
+              return ;
+          }
+          else{
+            this.searchParams.path=item.path;
+            this.searchParams.departmentId=item.id;
+            this.getMemberList(item);   
+          }
        },
-       change(){
-         
+       /* 搜索 */
+       search(){
+         if(this.currentItem.id&&this.currentItem.isShow){
+           this.getMemberList(this.currentItem);
+         }else{
+           this.$vux.toast.text('请至少选择一个部门');
+         }
+        
+
+       },
+       /* 获得列表 或 搜索项 */
+       getMemberList(obj){
+        if(obj.isShow){
+          this.$axios.get(this.memberListUrl,{
+            params:this.searchParams
+          }).then((res)=>{
+            if(res.data.code==1){
+              var arr=[];
+              arr=res.data.data.rows;
+              for(var i in arr)
+              arr[i].key=arr[i].id+'',
+              arr[i].value=arr[i].realname;
+              this.$nextTick(()=>{
+                               obj.childrenData=arr;
+                               this.currentItem=obj;
+              })
+              
+            }
+          })
+        }
+       },
+       /* 完成 */
+       submitChecked(){
+        var checked=[];
+
+       },
+       /* 选中改变 */
+       change(i,l){
+          this.currentItem.Checked=i;
+         console.log(this.treeData.sonDepartment);
        }
      }
  }   
@@ -93,9 +158,8 @@ import { Cell,CellBox,XHeader,Search,Group,Checklist  } from 'vux'
     .check_list{
       background-color: #fff;
       .check_list_li{
+        width:100%;
        .slide_box{
-         position: relative;
-         overflow: hidden;
         .check_item{
           transition: all 1s ease;
           .weui-cell{
@@ -109,9 +173,7 @@ import { Cell,CellBox,XHeader,Search,Group,Checklist  } from 'vux'
              }
           }
         }
-        .isHideList{
-         margin-top: -100%;
-        }
+
         }
 
       }
