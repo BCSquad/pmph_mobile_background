c213614db5901e477622e10d53b9ce728dbccf05
AzureCa <18428368747@sina.cn>
2018-03-27 16:57:02 +0800

    BUG修复

--- src/pages/home/material/selectEditor.vue
+++ src/pages/home/material/selectEditor.vue
@@ -8,7 +8,7 @@
      <div class="options_box" v-if="isShowList">
        <ul>
          <div class="arrow_box"></div>
-         <li>
+         <li @click="submit(1)">
            <i class="iconfont icon-wancheng"></i>
               确认
          </li>
@@ -16,7 +16,7 @@
            <i class="iconfont icon-yijianfankui"></i>
               发布
          </li> -->
-         <li>
+         <li @click="isShowDialog=true">
            <i class="iconfont icon-shijian"></i>
               历史记录
          </li>
@@ -40,62 +40,54 @@
          <span>出版社审核：{{publishState(item.offlineProgress)}}</span>
        </div>
        <div class="bottom_info" v-if="selectType=='chief'">
-            <div v-if="isArrowUp">
+            <div v-if="item.isArrowUp">
               <p><check-box v-model="item.isZhubian">是否主编</check-box></p>
               <x-input title="排位：" placeholder="" v-model.trim="item.zhubianSort" :show-clear="false"  ></x-input>
               <p><check-box v-model="item.isFuzhubian">是否副主编</check-box></p>
               <x-input title="排位：" placeholder=""  v-model.trim="item.fuzhubianSort"  :show-clear="false"  ></x-input>
             </div>
-            <div class="grey_check_box" v-if="!isArrowUp">
-              <p><check-box v-model="demo2" >是否主编</check-box></p>
+            <div class="grey_check_box" v-if="!item.isArrowUp">
+              <p><check-box v-model="item.isZhubian" >是否主编</check-box></p>
             </div>
        </div>
        <div class="bottom_info" v-if="selectType=='editor'">
-          <div v-if="isArrowUp">
+          <div v-if="item.isArrowUp">
               <p><check-box v-model="item.isZhubian">是否主编</check-box></p>
               <p><check-box v-model="item.isDigitalEditor">是否数字编委</check-box></p>
             </div>
-            <div class="grey_check_box" v-if="!isArrowUp">
+            <div class="grey_check_box" v-if="!item.isArrowUp">
               <p><check-box v-model="item.isZhubian" >是否主编</check-box> <span style="float:right;color:#606266">排位：{{item.rank}}</span></p>
             </div>
        </div>
        <div class="arrow_box">
-            <p><span :class="{'to_down':!isArrowUp}" @click="isArrowUp=!isArrowUp"></span></p>
+            <p><span :class="{'to_down':!item.isArrowUp}" @click="item.isArrowUp=!item.isArrowUp"></span></p>
        </div>
-      </li>
-      <!-- <li>
-       <div class="top_info">
-         <p>张三<span>学校审核:待审核</span></p>
-         <span>申报单位：内蒙古科技大学</span>
-         <span>申请职位：编委</span>
-         <span>出版社审核：已收到纸质表</span>
-       </div>
-       <div class="bottom_info">
-            <div v-if="isArrowUp">
-              <p><check-box v-model="demo2">是否主编</check-box></p>
-              <x-input title="排位：" placeholder=""  :show-clear="false"  ></x-input>
-              <p><check-box v-model="demo2">是否副主编</check-box></p>
-              <x-input title="排位：" placeholder=""  :show-clear="false"  ></x-input>
-            </div>
-            <div class="grey_check_box" v-if="!isArrowUp">
-              <p><check-box v-model="demo2" >是否副主编</check-box></p>
-            </div>
-       </div>
-       <div class="arrow_box">
-            <p><span :class="{'to_down':!isArrowUp}" @click="isArrowUp=!isArrowUp"></span></p>
-       </div>
-      </li> -->      
+      </li>   
     </ul>
     <load-more :show-loading="false" tip="暂无数据" ></load-more>
+
+      <x-dialog v-model="isShowDialog"  hide-on-blur>
+        <div class="history-box timeLine">
+          <ul v-if="historyLog.length>0">
+            <li v-for="(iterm,index) in historyLog" :key="index">
+              <b></b>
+              <p v-for="(item,index) in iterm.detail" :key="index">{{item}}</p>
+            </li>
+          </ul>
+          <p v-else>暂无历史消息</p>
+        </div>
+      </x-dialog>    
   </div>
 </template>
 <script>
-import { Cell,CellBox,XHeader,Search,CheckIcon,XInput,LoadMore  } from 'vux'
+import { Cell,CellBox,XHeader,Search,CheckIcon,XInput,LoadMore,XDialog  } from 'vux'
 import CheckBox from '../../../components/checkbox'
  export default{
      data(){
          return{
            listUrl:'/pmpheep/declaration/list/editor/selection',  //列表url
+           api_submit:'/pmpheep/declaration/editor/selection/update',
+           api_log:'/pmpheep/textBookLog/list',
            listData:[],
            bookName:'',
            searchParams:{
@@ -104,15 +96,16 @@ import CheckBox from '../../../components/checkbox'
               orgName:'',
               materialId:'',
            },
+           historyLog:[],
            isShowList:false,
+           isShowDialog:false,
            selectType:'chief',
            IsDigitalEditorOptional:false,
-           demo2:false,
            isArrowUp:false
          }
      },
      components: {
-       Cell,CellBox,XHeader,Search,CheckIcon,CheckBox,XInput,LoadMore
+       Cell,CellBox,XHeader,Search,CheckIcon,CheckBox,XInput,LoadMore,XDialog
      },
      created(){
         if(this.$route.params.materialId){
@@ -122,6 +115,7 @@ import CheckBox from '../../../components/checkbox'
           this.bookName=this.$route.params.bookName;
         }
          this.getList();
+         this.getHistoryLog();
      },
      methods:{
        /* 获取列表数据 */
@@ -147,6 +141,8 @@ import CheckBox from '../../../components/checkbox'
                 iterm.isBianwei = (iterm.chosenPosition%8)==1;
                 iterm.isDigitalEditor = iterm.chosenPosition>=8;
 
+                /* 下拉参数 */ 
+                 iterm.isArrowUp=false; 
 
                 iterm.disabled_zb = this.type=='bw'||iterm.isBianwei;
                 iterm.disabled_bw = this.type=='zb'||(iterm.isZhubian||iterm.isFuzhubian);
@@ -158,6 +154,28 @@ import CheckBox from '../../../components/checkbox'
           }
         })
        },
+       /* 获取历史记录 */
+      getHistoryLog(){
+        this.$axios.get(this.api_log,{params:{
+          textbookId:this.searchParams.textbookId,
+          pageNumber:1,
+          pageSize:1000
+        }})
+          .then(response=>{
+            var res = response.data;
+            if(res.code==1){
+              /* res.data.rows.push({id: 99, detail: "[测试人员] 于 [今天 15:18];增加了1位主编:[王帅]"}); */
+              res.data.rows.forEach(iterm=>{
+                iterm.detail = iterm.detail.split(';');
+              });
+              this.historyLog = res.data.rows;
+              console.log(this.historyLog);
+            }
+          })
+          .catch(e=>{
+            console.log(e);
+          })
+      },
        /* 学校审核状态区分 */
        initState(i){
         switch (i) {
@@ -192,6 +210,65 @@ import CheckBox from '../../../components/checkbox'
            default:
              break;
          }
+       },
+       submit(type){
+         const _this=this;
+          this.$vux.confirm.show({
+            title: '提示',
+            content: '确认提交？',
+            onConfirm () {
+              let jsonDecPosition = [];
+              for(let i = 0, len = _this.listData.length; i < len; i++){
+                _this.listData[i].chosenPosition = (_this.listData[i].isDigitalEditor?8:0)+(_this.listData[i].isZhubian?4:0)+(_this.listData[i].isFuzhubian?2:0)+(_this.listData[i].isBianwei?1:0);
+                let tempObj = {
+                  id:_this.listData[i].id,
+                  textbookId:_this.searchParams.textbookId,
+                  chosenPosition:_this.listData[i].chosenPosition,
+                  declarationId:_this.listData[i].declarationId,
+                  presetPosition:_this.listData[i].presetPosition,
+                  syllabusId:_this.listData[i].syllabusId,
+                  syllabusName:_this.listData[i].syllabusName,
+                  rank:(_this.listData[i].chosenPosition%8)==4?_this.listData[i].zhubianSort:((_this.listData[i].chosenPosition%8)==2?_this.listData[i].fuzhubianSort:'')
+                };
+                if(_this.listData[i].isZhubian||_this.listData[i].isFuzhubian||_this.listData[i].isBianwei||_this.listData[i].isDigitalEditor){
+                  jsonDecPosition.push(tempObj);
+                }
+              }
+  //            console.log(jsonDecPosition);
+              //提交
+              _this.$axios.put(_this.api_submit,_this.$commonFun.initPostData({
+                jsonDecPosition:JSON.stringify(jsonDecPosition),
+                selectionType:type?type:1,
+                textbookId: _this.searchParams.textbookId,
+                editorOrEditorialPanel:_this.type=='zb'?1:2,
+                unselectedHold:_this.type=='zb'?1:(jsonDecPosition.length)>0?1:0
+              }))
+                .then(response=>{
+                  var res = response.data;
+                  _this.isShowList=false;
+                  if(res.code==1){
+                    if(type===2){
+                      _this.$router.go(-1);
+                    }else{
+                      _this.getList();
+                    }
+                    _this.$vux.toast.show({
+                              text: '提交成功！'
+                            });
+                  }else{
+                    _this.$vux.toast.show({
+                          text: res.msg.msgTrim(),
+                          type:'cancel'
+                        });
+                  }
+                })
+                .catch(e=>{
+                  console.log(e);
+                })
+
+
+            }
+          })
        }
      }
  }   
@@ -348,6 +425,45 @@ import CheckBox from '../../../components/checkbox'
        }
      }
    }
+
+.history-box{
+    min-height: 300px;
+    padding:20px;
+    box-sizing: border-box;
+  }
+.timeLine {
+    display: block;
+    margin: 15px 0;
+    ul{
+          margin-left: 25px;
+          border-left: 2px solid #ddd;
+          li{
+                min-height: 30px;
+                margin-bottom: 30px;
+                width: 100%;
+                margin-left: -12px;
+                line-height: 20px;
+                font-weight: normal;
+                b{
+                  width: 8px;
+                  height: 8px;
+                  background: #fff;
+                  border: 2px solid #555;
+                  margin: 5px;
+                  border-radius: 6px;
+                  -webkit-border-radius: 6px;
+                  -moz-border-radius: 6px;
+                  overflow: hidden;
+                  display: inline-block;
+                  float: left;
+                }
+                p{
+                  text-align: left;
+                  padding-left:25px;
+                }
+          }
+    }
+}
 }
 
 </style>
