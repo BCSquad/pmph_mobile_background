653e5ae1d583cef8f0807f075745789cb31e7791
xiaochengyan <985814585@qq.com>
2018-05-03 10:24:32 +0800

    选题申报-申报表审核-编辑审核-审核详情页面--无法分配，无法回退、无法审核

--- src/pages/home/selectTopic/opinionInput.vue
+++ src/pages/home/selectTopic/opinionInput.vue
@@ -4,17 +4,36 @@
     <group>
       <x-textarea  :max="100" class="opinion_textarea" v-model="params.authFeedback"></x-textarea>
     </group>
-    <x-button  type="primary"  style="background-color:#FF9040;" class="button_input" @click.native="check(3)">通过</x-button>
-    <!-- <x-button type="primary"   style="background-color:#0fb295" class="button_input">退回分配人</x-button> -->
+    <x-button  type="primary"  style="background-color:#FF9040;" class="button_input" @click.native="check(3)">通过并提交ERP</x-button>
+     <x-button type="primary"   style="background-color:#0fb295" class="button_input"  @click.native="backAccept()">退回分配人</x-button>
     <x-button  type="primary" style="background-color:#303133" class="button_input" @click.native="check(2)">不通过</x-button>
+
+    <!-- 退回原因弹框 -->
+    <confirm v-model="showBackConfirm"
+             show-input
+             title="请填写退回原因"
+             @on-confirm="backConfirm"
+             placeholder="请输入退回原因"
+             :input-attrs="{type: 'textarea'}"
+    >
+    </confirm>
   </div>
 </template>
 <script>
-import { XTextarea, Group,XButton,Box} from 'vux'
+import { XTextarea, Group,XButton,Box,Confirm} from 'vux'
     export default{
         data(){
             return{
               params:{},
+              currentBackId:'',
+              showBackConfirm:false,
+              acceptToUrl:'/pmpheep/topic/put/editorHandling',       //受理url
+              acceptParams:{
+                id:'',
+                isAccepted:'',
+                isRejectedByEditor:'',
+                reasonEditor:''
+              }
             }
         },
         created(){
@@ -25,7 +44,7 @@ import { XTextarea, Group,XButton,Box} from 'vux'
            }
         },
         components: {
-                  XTextarea, Group,XButton,Box
+                  XTextarea, Group,XButton,Box,Confirm
               },
         methods:{
                       // 审核
@@ -39,10 +58,12 @@ import { XTextarea, Group,XButton,Box} from 'vux'
               )
               .then(response => {
                 let res = response.data;
+
                 if (res.code == "1") {
                   this.$vux.toast.show({
                             text: "操作成功！"
-                      })
+                      });
+                  this.$router.push({name:'选题申报列表',query:{TopicType:3}});
                 } else {
                    this.$vux.toast.show({
                             text: res.data.msg.msgTrim(),
@@ -54,6 +75,35 @@ import { XTextarea, Group,XButton,Box} from 'vux'
 
               });
           },
+          acceptApi(str){
+            this.$axios.put(this.acceptToUrl,this.$commonFun.initPostData(this.acceptParams))
+              .then((res)=>{
+                if(res.data.code==1){
+                  this.$vux.toast.show({
+                    text: str=='accept'?'已成功受理':'已退回'
+                  })
+                  this.$router.push({name:'选题申报列表',query:{TopicType:2}});
+                }else{
+
+                }
+              })
+          },
+          /* 退回点击 */
+          backAccept(){
+            this.currentBackId=this.params.id;
+            this.showBackConfirm=true;
+          },
+          /* 退回确定 */
+          backConfirm(msg){
+
+              this.acceptParams={
+                id:this.currentBackId,
+                isRejectedByEditor:true,
+                reasonEditor:msg
+              }
+              this.acceptApi('back');
+
+          }
               }
     }
 </script>
