0730d7e40906a92c949b5a9f8780753616a135a6
xiaochengyan <985814585@qq.com>
2018-05-10 09:09:18 +0800

    1632 分配部门未实现，且与实际不符

--- src/router/index.js
+++ src/router/index.js
@@ -153,7 +153,7 @@ export default new Router({
                         { path: 'progress', name: '选题进度查询', component: QueryProgress, meta: { title: '选题进度查询' } },
                         { path: 'list', name: '选题申报列表', component: TopicList, meta: { title: '我要出书' } },
                         { path: 'editor', name: '分配编辑', component: DistributeEditor, meta: { title: '分配编辑' } },
-                        { path: 'department', name: '分配部门', component: DistributeDepartment, meta: { title: '分配部门' } },
+                        { path: 'department', name: '分配部门', component: DistributeDepartment, meta: { title: '分配部门', hideTopBar: true } },
                         { path: 'exam', name: '申报表审核', component: ExamDetail, meta: { title: '我要出书', hideTopBar: true, } },
                         { path: 'input', name: '审核意见录入', component: OpinionInput, meta: { title: '申报表审核' } },
                     ]
