e74fe087798044c0f40dd0a1f8209491e4ad8ec3
xiaochengyan <985814585@qq.com>
2018-05-10 15:53:45 +0800

    2954 选题审核流程问题

--- src/pages/home/selectTopic/opinionInput.vue
+++ src/pages/home/selectTopic/opinionInput.vue
@@ -5,7 +5,7 @@
       <x-textarea  :max="100" class="opinion_textarea" v-model="params.authFeedback"></x-textarea>
     </group>
     <x-button  type="primary"  style="background-color:#FF9040;" class="button_input" @click.native="check(3)">通过</x-button>
-     <x-button type="primary"   style="background-color:#0fb295" class="button_input"  @click.native="backAccept()">退回分配人</x-button>
+     <x-button type="primary"   style="background-color:#0fb295" class="button_input"  @click.native="backAccept(params.isAccepted,params.id)">退回分配人</x-button>
     <x-button  type="primary" style="background-color:#303133" class="button_input" @click.native="check(2)">不通过</x-button>
 
     <!-- 退回原因弹框 -->
@@ -27,13 +27,20 @@ import { XTextarea, Group,XButton,Box,Confirm} from 'vux'
               params:{},
               currentBackId:'',
               showBackConfirm:false,
+              distributeUrl:'/pmpheep/topic/put/directorHandling' ,       //分配编辑 或 退回分配人url
               acceptToUrl:'/pmpheep/topic/put/editorHandling',       //受理url
               acceptParams:{
                 id:'',
                 isAccepted:'',
                 isRejectedByEditor:'',
                 reasonEditor:''
-              }
+              },
+              distributeParams:{
+                id:'',
+                editorId:'',
+                isRejectedByDirector:'',
+                reasonDirector:''
+              },
             }
         },
         created(){
@@ -89,21 +96,45 @@ import { XTextarea, Group,XButton,Box,Confirm} from 'vux'
               })
           },
           /* 退回点击 */
-          backAccept(){
-            this.currentBackId=this.params.id;
-            this.showBackConfirm=true;
+          backAccept(isAccepted,id){
+            if(isAccepted){
+              this.$vux.toast.show({
+                text: '该选题已被受理，请勿退回分配人',
+                type:'cancel'
+              })
+              this.currentBackId=id;
+              this.showBackConfirm=false;
+              return ;
+            }else{
+
+              this.currentBackId=id;
+              this.showBackConfirm=true;
+            }
+          },/* 退回分配人 */
+          backAssigner(id,msg){
+            const _this = this;
+            _this.distributeParams.id=id;
+            _this.distributeParams.isRejectedByDirector=true;
+            _this.distributeParams.reasonDirector=msg;
+            _this.$axios.put(_this.distributeUrl,_this.$commonFun.initPostData(_this.distributeParams))
+              .then((res)=>{
+                if(res.data.code==1){
+                  _this.$vux.toast.show({
+                    text: '退回成功'
+                  })
+                }else{
+                  _this.$vux.toast.show({
+                    text: res.data.msg.msgTrim(),
+                    type:'cancel'
+                  })
+                }
+              })
           },
           /* 退回确定 */
           backConfirm(msg){
+            this.backAssigner(this.params.id,msg);
+          },
 
-              this.acceptParams={
-                id:this.currentBackId,
-                isRejectedByEditor:true,
-                reasonEditor:msg
-              }
-              this.acceptApi('back');
-
-          }
               }
     }
 </script>
