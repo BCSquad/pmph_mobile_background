c61be175d8497ab9af87af80e2ce3247a96d67e5
liudi-aditenmin <swadia_liudi@163.com>
2018-05-24 09:38:51 +0800

    3052 职位遴选：选项改变  指派 激活 重现步骤 [步骤] 作家已经提交教材申报  书籍和职位多选。如果后台  教材申报修改为单选，作家提交过的状态应不变

--- src/pages/home/material/expert-manage-book.vue
+++ src/pages/home/material/expert-manage-book.vue
@@ -45,7 +45,7 @@
               <Radio label="编委" class="block marginL0" >编委</Radio>
               <Radio label="数字编委" class="block marginL0"  v-if="searchParams.isDigitalEditorOptional">数字编委</Radio>
             </RadioGroup>-->
-            <checklist v-if="!searchParams.isMultiPosition" :max=1 :options="pos"  v-model="item.showPosition" :disabled="!item.isNew"></checklist>
+            <checklist v-if="!searchParams.isMultiPosition&&item.isNew" :max=1 :options="pos"  v-model="item.showPosition" :disabled="!item.isNew"></checklist>
             <checklist v-else :options="pos" v-model="item.showPosition" :disabled="!item.isNew" ></checklist>
           </div>
           <p class="ellipsis book-file">
