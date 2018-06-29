f334054e4b061ea72b1149fe1a86cfb08666417f
liudi-aditenmin <swadia_ld@163.com>
2018-06-29 18:12:53 +0800

    地址

--- config/index.js
+++ config/index.js
@@ -43,8 +43,8 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
-
+        //assetsPublicPath: '/wx', //120
+        assetsPublicPath: '/pmphvuewx',//119
         /**
          * Source Maps
          */
